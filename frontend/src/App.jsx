6d025c9c8bd39abc52331f6c0b958513aea29db9
Sarthak <s.sarthak1357@gmail.com>
2026-05-31 17:32:19 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,31 @@
+import './App.css'
+import Nav from './components/Nav'
+import Footer from './components/Footer'
+import { Route, Routes } from 'react-router-dom'
+import SignUp from './components/SignUp'
+import PrivateComponent from './components/PrivateComponent'
+import Login from './components/Login'
+
+function App() {
+
+  return (
+    <>
+    <Nav/>
+    <Routes>
+      <Route element={<PrivateComponent/>}>
+      <Route path="/" element={<h1>Product Listing Document</h1>}/>
+      <Route path="/add" element={<h1>Product Add Document</h1>}/>
+      <Route path="/update" element={<h1>Product Update Document</h1>}/>
+      <Route path="/logout" element={<h1>Product Logout Document</h1>}/>
+      <Route path="/profile" element={<h1>Profile Document</h1>}/>
+      </Route>
+      
+      <Route path="/signup" element={<SignUp />} />
+      <Route path='/login' element={<Login/>} />
+    </Routes>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/components/SignUp.jsx
@@ -0,0 +1,45 @@
+import React, { Component, useEffect } from 'react'
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const SignUp=()=>{
+    const [name,setName]=useState("");
+    const [email,setEmail]=useState("");
+    const [password,setPassword]=useState("");
+    const navigate = useNavigate();
+
+    useEffect(()=>{
+        const auth = localStorage.getItem('user');
+        if(auth)
+        {
+            navigate('/');
+        }
+    },[])
+
+    const collectData=async ()=>{
+        console.warn(name,email,password);
+        let result = await  fetch('http://localhost:5000/register',{
+            method:'POST',
+            body:JSON.stringify({name,email,password}),
+            headers:{
+                'Content-Type':'application/json'
+            },
+        });
+        result = await result.json();
+        //console.log(result);
+        localStorage.setItem("user",JSON.stringify(result));
+        navigate('/');
+    }
+
+    return(
+        <div className='input'>
+            <h1>Register</h1>
+            <input type="text" value={name} onChange={(e)=>setName(e.target.value)} placeholder="Enter Name" />
+            <input type="text" value={email} onChange={(e)=>setEmail(e.target.value)} placeholder="Enter Email" />
+            <input type="password" value={password} onChange={(e)=>setPassword(e.target.value)} placeholder="Enter Password" />
+            <button type="button" className='appbutton' onClick={collectData}>SignUp</button>
+        </div>
+    )
+}
+
+export default SignUp;
